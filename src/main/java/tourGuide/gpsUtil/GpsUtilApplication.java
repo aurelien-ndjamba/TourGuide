8de564c8b1f2643b7c6df1b9be4058b81bc79a77
andjamba <aurelien.ndjamba@gmail.com>
2022-03-16 23:00:28 +0100

    API gpsUtil finalisée

--- src/main/java/tourGuide/gpsUtil/GpsUtilApplication.java
+++ src/main/java/tourGuide/gpsUtil/GpsUtilApplication.java
@@ -1,5 +1,7 @@
 package tourGuide.gpsUtil;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,7 @@ import gpsUtil.GpsUtil;
 public class GpsUtilApplication {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US); 
 		SpringApplication.run(GpsUtilApplication.class, args);
 	}
 	
--- src/main/java/tourGuide/gpsUtil/service/GpsUtilService.java
+++ src/main/java/tourGuide/gpsUtil/service/GpsUtilService.java
@@ -9,13 +9,15 @@ import org.springframework.stereotype.Service;
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
+import lombok.Data;
 
+@Data
 @Service
 public class GpsUtilService {
-	
+
 	@Autowired
 	private GpsUtil gpsUtil;
-	
+
 	public VisitedLocation getUserLocation(UUID userId) {
 		return gpsUtil.getUserLocation(userId);
 	}
